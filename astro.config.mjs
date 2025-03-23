971add0d98b086429cb85b21cfd481ceacecd6dd
Tim Huntley <timshuntley@gmail.com>
2025-03-23 09:58:00 -0400

    Added 404 page and robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: "https://reinnovationsoftware.com",
 	integrations: [mdx(), sitemap()],
 });
